73545c3df6676eff632d613b1574f27aeb3561de
Alex Gidiotis <gidiotisAlex@outlook.com.gr>
2017-11-30 18:04:27 +0200

    added multi-label support

--- classifier.py
+++ classifier.py
@@ -18,6 +18,8 @@ from keras.callbacks import EarlyStopping, ModelCheckpoint
 from keras.constraints import maxnorm
 
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MultiLabelBinarizer
+
 
 # Modify this paths as well
 DATA_DIR = '/home/alex/Documents/git_projects/Document-Classifier-LSTM/data/'
@@ -36,44 +38,44 @@ STAMP = 'doc_blstm'
 
 
 class Corpus(object):
-    # Data generator.
-    # INitialize the input files.
+	# Data generator.
+	# INitialize the input files.
 	def __init__(self,in_file,target_file=None):
 		self.in_file = in_file
 		self.target_file = target_file
 		self.__iter__()
 
-    # Yield one row,target pair per iteration. Each row is an abstract (preprocessed)
+	# Yield one row,target pair per iteration. Each row is an abstract (preprocessed)
 	def __iter__(self):
 		for i,(line,target_list) in enumerate(zip(open(self.in_file),open(self.target_file))):
-            # We train using only the first of possibly multiple classes.
-			yield ' '.join(line.strip().replace('-',' ').split(',')),target_list.strip().split(',')[0]
+			# We train using only the first of possibly multiple classes.
+			yield ' '.join(line.strip().replace('-',' ').split(',')),target_list.strip().split(',')
 
 
 #============================================================== Prepare word embeddings ===========================================================
 def prepare_embeddings(word_index): 
-    # Returns the embedding matrix for the words in our corpus.
-    print('Preparing embedding matrix')
-    print('Indexing word vectors')
-    # Read the pre-trained embeddings.
-    word2vec = KeyedVectors.load_word2vec_format(EMBEDDING_FILE,
-        binary=True)
-    print('Found %s word vectors of word2vec' % len(word2vec.vocab))
+	# Returns the embedding matrix for the words in our corpus.
+	print('Preparing embedding matrix')
+	print('Indexing word vectors')
+	# Read the pre-trained embeddings.
+	word2vec = KeyedVectors.load_word2vec_format(EMBEDDING_FILE,
+		binary=True)
+	print('Found %s word vectors of word2vec' % len(word2vec.vocab))
 
 
-    # Fill the embedding matrix.
-    nb_words = min(MAX_NB_WORDS, len(word_index))
-    embedding_matrix = np.zeros((nb_words, EMBEDDING_DIM))
-    for word, i in word_index.items():
-        if i >= MAX_NB_WORDS: continue
-        if word in word2vec.vocab:
-            embedding_matrix[i] = word2vec.word_vec(word)
-    # WOrds without embeddings are left with zeros.
-    print('Null word embeddings: %d' % np.sum(np.sum(embedding_matrix,
-        axis=1) == 0))
+	# Fill the embedding matrix.
+	nb_words = min(MAX_NB_WORDS, len(word_index))
+	embedding_matrix = np.zeros((nb_words, EMBEDDING_DIM))
+	for word, i in word_index.items():
+		if i >= MAX_NB_WORDS: continue
+		if word in word2vec.vocab:
+			embedding_matrix[i] = word2vec.word_vec(word)
+	# WOrds without embeddings are left with zeros.
+	print('Null word embeddings: %d' % np.sum(np.sum(embedding_matrix,
+		axis=1) == 0))
 
 
-    return embedding_matrix, nb_words
+	return embedding_matrix, nb_words
 
 
 #============================================================= Main function =====================================================================
@@ -88,24 +90,35 @@ for c,(vector,target) in enumerate(train_set):  # load one vector into memory at
 	if c % 10000 == 0: 
 		print c
 
+	if c > 200000: 
+		break
+
 print len(X_data), 'training examples'
 
-
 # Dictionary of classes.
-class_list = list(set(y_data))
+class_list = list(set([y for y_seq in y_data for y in y_seq]))
+
 nb_classes = len(class_list)
 print nb_classes,'classes'
 class_dict = dict(zip(class_list, np.arange(len(class_list))))
 with open('class_dict.json', 'w') as fp:
-    json.dump(class_dict, fp)
+	json.dump(class_dict, fp)
 print 'Exported class dictionary'
 
 
 # Prepare the labels for training.
-y_data = np.array([class_dict[y] for y in y_data])
-y_data = to_categorical(y_data,
-    num_classes=nb_classes)
+y_data_int = []
+for y_seq in y_data:
+	y_data_int.append([class_dict[y] for y in y_seq])
 
+y_data = np.array(y_data_int)
+
+mlb = MultiLabelBinarizer()
+y_data = mlb.fit_transform(y_data)
+'''
+y_data = to_categorical(y_data,
+	num_classes=nb_classes)
+'''
 
 # Tokenize and pad text.
 tokenizer = Tokenizer(num_words=MAX_NB_WORDS)
@@ -114,10 +127,10 @@ X_data = tokenizer.texts_to_sequences(X_data)
 word_index = tokenizer.word_index
 print('Found %s unique tokens' % len(word_index))
 X_data = pad_sequences(X_data,
-    maxlen=MAX_SEQUENCE_LENGTH,
-    padding='post',
-    truncating='post',
-    dtype='float32')
+	maxlen=MAX_SEQUENCE_LENGTH,
+	padding='post',
+	truncating='post',
+	dtype='float32')
 print('Shape of data tensor:', X_data.shape)
 print('Shape of label tensor:', y_data.shape)
 
@@ -126,48 +139,48 @@ print('Shape of label tensor:', y_data.shape)
 embedding_matrix, nb_words = prepare_embeddings(word_index)
 #======================================================= Split the data into train/val sets =======================================================
 X_train, X_val, y_train, y_val = train_test_split(X_data, y_data,
-    test_size=0.1,
-    random_state=42)
+	test_size=0.1,
+	random_state=42)
 
 #============================================================ Build the model =====================================================================
 embedding_layer = Embedding(nb_words,
-        EMBEDDING_DIM,
-        weights=[embedding_matrix],
-        input_length=MAX_SEQUENCE_LENGTH,
-        trainable=False)
+		EMBEDDING_DIM,
+		weights=[embedding_matrix],
+		input_length=MAX_SEQUENCE_LENGTH,
+		trainable=False)
 
 blstm_layer = Bidirectional(LSTM(300,
-    activation='tanh',
-    recurrent_activation='hard_sigmoid',
-    recurrent_dropout=0.0,
-    dropout=0.2, 
-    kernel_constraint=maxnorm(3),
-    kernel_initializer='glorot_uniform'),
-    merge_mode='concat')
+	activation='tanh',
+	recurrent_activation='hard_sigmoid',
+	recurrent_dropout=0.0,
+	dropout=0.2, 
+	kernel_constraint=maxnorm(3),
+	kernel_initializer='glorot_uniform'),
+	merge_mode='concat')
 
 sequence_input = Input(shape=(MAX_SEQUENCE_LENGTH,),
-    dtype='int32')
+	dtype='int32')
 embedded_sequences = embedding_layer(sequence_input)
 blstm_1 = blstm_layer(embedded_sequences)
 
 dense_1 = Dropout(0.5)(blstm_1)
 dense_1 = BatchNormalization()(dense_1)
 dense_1 = Dense(200,
-    activation='relu',
-    kernel_initializer='glorot_uniform')(dense_1)
+	activation='relu',
+	kernel_initializer='glorot_uniform')(dense_1)
 
 dense_2 = Dropout(0.5)(dense_1)
 dense_2 = BatchNormalization()(dense_2)
 
 preds = Dense(nb_classes,
-    activation='softmax')(dense_2)
+	activation='sigmoid')(dense_2)
 
 #=========================================================== Train the model ===================================================================
 model = Model(inputs=[sequence_input],
-    outputs=preds)
-model.compile(loss='categorical_crossentropy',
-    optimizer='adam',
-    metrics=['acc'])
+	outputs=preds)
+model.compile(loss='binary_crossentropy',
+	optimizer='adam',
+	metrics=['categorical_accuracy'])
 
 model.summary()
 print(STAMP)
@@ -176,22 +189,22 @@ print(STAMP)
 # Save the model.
 model_json = model.to_json()
 with open(STAMP + ".json", "w") as json_file:
-    json_file.write(model_json)
+	json_file.write(model_json)
 
 
 early_stopping =EarlyStopping(monitor='val_loss',
-    patience=3)
+	patience=5)
 bst_model_path = STAMP + '.h5'
 model_checkpoint = ModelCheckpoint(bst_model_path,
-    monitor='val_acc',
-    verbose=1,
-    save_best_only=True,
-    save_weights_only=True)
+	monitor='val_categorical_accuracy',
+	verbose=1,
+	save_best_only=True,
+	save_weights_only=True)
 
 
 hist = model.fit(X_train, y_train,
-    validation_data=(X_val, y_val),
-    epochs=200,
-    batch_size=256,
-    shuffle=True,
-    callbacks=[early_stopping, model_checkpoint])
+	validation_data=(X_val, y_val),
+	epochs=200,
+	batch_size=256,
+	shuffle=True,
+	callbacks=[early_stopping, model_checkpoint])
